fb3af951e9fe3b70650a15661c99960b1c316435
mathiasBaumgartinger <mathias.baumgartinger@proton.me>
2025-07-29 14:29:09 +0200

    feat/WIP(entry-definition): draft for django-djson form for entry-definitions
    TODO: needs adjustments when migrating and in frontend as structure changed minimally (question is now a field inside an array as opposed to the key of an object).

--- webgis/models.py
+++ webgis/models.py
@@ -1,6 +1,6 @@
 from djgeojson.fields import PointField, MultiPointField
 from django.db import models
-from django.contrib.postgres.fields import ArrayField
+from django_jsonform.models.fields import JSONField
 
 
 #
@@ -13,10 +13,166 @@ class EntryDefinitionManager(models.Manager):
 
 
 class EntryDefinition(models.Model):
+    FORM_TYPES = [
+        "Comment",
+        "MultiLineText",
+        "Dropdown",
+        "Spinbox",
+        "Image",
+        "Radio",
+        "Checkbox",
+        "SemanticDifferential",
+        "MatrixQuestions",
+    ]
+    
+    PARAMS_SCHEMA = {
+        "Comment_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "Comment" },
+            },
+            "additionalProperties": False
+        },
+        "MultiLineText_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "MultiLineText" },
+            },
+            "additionalProperties": False
+        },
+        "Dropdown_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "Dropdown" },
+                "values": {
+                    "type": "array",
+                    "title": "Options",
+                    "minItems": 2,
+                    "items": {"type": "string"}
+                }
+            },
+            "required": ["values"],
+            "additionalProperties": False
+        },
+        "Spinbox_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "Spinbox" },
+                "min_value": {"type": "number"},
+                "max_value": {"type": "number"},
+                "step": {"type": "number"},
+                "value": {"type": "number"}
+            },
+            "required": ["min", "max", "step", "value"],
+            "additionalProperties": False
+        },
+        "Image_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "Image" },    
+            },
+            "additionalProperties": False
+        },
+        "Radio_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "Radio" },
+                "values": {
+                    "type": "array",
+                    "items": {"type": "string"}
+                }
+            },
+            "required": ["values"],
+            "additionalProperties": False
+        },
+        "Checkbox_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "Checkbox" },
+                "values": {
+                    "type": "array",
+                    "items": {"type": "string"}
+                }
+            },
+            "required": ["values"],
+            "additionalProperties": False
+        },
+        "SemanticDifferential_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "SemanticDifferential" },
+                "scales": {
+                    "type": "array",
+                    "items": {"type": "string"}
+                },
+                "rowDefinitions": {
+                    "type": "array",
+                    "items": {
+                        "type": "array",
+                        "items": {"type": "string"},
+                        "minItems": 2,
+                        "maxItems": 2,
+                    }
+                }
+            }
+        },
+        "MatrixQuestions_params": {
+            "type": "object",
+            "properties": {
+                "kind": { "const": "MatrixQuestions" },
+                "scales": {
+                    "type": "array",
+                    "items": {"type": "string"}
+                },
+                "questions": {
+                    "type": "array",
+                    "items": {
+                        "type": "string"
+                    }
+                }
+            }
+        }
+    }
+    
+    DEFINITION_SCHEMA = {
+        "type": "array",
+        "items": {
+            "type": "dict",
+            "keys": {
+                "question": {
+                    "type": "string"
+                },
+                "description": {
+                    "type": "string",
+                    "helpText": "(Optionally) explain the user how to operate this field and what it means (e.g. \"1... very dissatisfactory, 5... very satisfactory\")"
+                },
+                "type": {
+                    "type": "string",
+                    "choices": FORM_TYPES
+                },
+                "params": {
+                    "oneOf": [
+                        { "title": "Checkbox", "$ref": "#/$defs/Checkbox_params", "default": {"kind": "Checkbox"} },
+                        { "title": "Comment", "$ref": "#/$defs/Comment_params", "default": {"kind": "Comment"} },
+                        { "title": "MultiLineText", "$ref": "#/$defs/MultiLineText_params", "default": {"kind": "MultiLineText"} },
+                        { "title": "Dropdown", "$ref": "#/$defs/Dropdown_params", "default": {"kind": "Dropdown"} },
+                        { "title": "Spinbox", "$ref": "#/$defs/Spinbox_params", "default": {"kind": "Spinbox"} },
+                        { "title": "Image", "$ref": "#/$defs/Image_params", "default": {"kind": "Image"} },
+                        { "title": "Radio", "$ref": "#/$defs/Radio_params", "default": {"kind": "Radio"} },
+                        
+                        { "title": "SemanticDifferential", "$ref": "#/$defs/SemanticDifferential_params", "default": {"kind": "SemanticDifferential"} },
+                        { "title": "MatrixQuestions", "$ref": "#/$defs/MatrixQuestions_params", "default": {"kind": "MatrixQuestions"} },
+                    ]
+                },
+            },
+            "required": ["question", "type"]
+        },
+        "$defs": PARAMS_SCHEMA
+    }
     
     objects = EntryDefinitionManager()
     name = models.CharField(max_length=256)
-    field_definition = models.JSONField(db_index=True)
+    field_definition = JSONField(schema=DEFINITION_SCHEMA, db_index=True)
 
     def natural_key(self):
         return (self.name, self.field_definition)
